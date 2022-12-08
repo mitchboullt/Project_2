5a7ec4f799122c70142268ed7f05744e3e98a721
mitchboullt <112483018+mitchboullt@users.noreply.github.com>
2022-12-08 18:50:42 -0500

    cleaned csvs and sql queries

--- Movie_Data/Notebook/queries.sql
+++ Movie_Data/Notebook/queries.sql
@@ -1,7 +1,7 @@
--- Create tables for raw data to be loaded into
 CREATE TABLE movie_money (
 rank INT PRIMARY KEY,
 title text,
+genre text,
 production_cost bigint,
 worldwide_gross bigint,
 theaters INT
@@ -9,7 +9,6 @@ theaters INT
 
 CREATE TABLE movie_info (
 title text,
-genre text,
 IMDB_Rating int,
 Meta_score INT,
 Director text,
@@ -18,7 +17,7 @@ Star2 text,
 No_of_Votes int
 );
 
-SELECT movie_money.rank, movie_money.title, movie_money.production_cost, movie_money.worldwide_gross, movie_money.theaters, movie_info.title, movie_info.genre, movie_info.imdb_rating,movie_info.genre,movie_info.meta_score,movie_info.director,movie_info.star1,movie_info.star2,movie_info.no_of_votes
+SELECT movie_money.rank, movie_money.title, movie_money.production_cost, movie_money.worldwide_gross, movie_money.theaters, movie_money.genre, movie_info.imdb_rating,movie_info.meta_score,movie_info.director,movie_info.star1,movie_info.star2,movie_info.no_of_votes
 FROM movie_money
 JOIN movie_info
 ON movie_money.title = movie_info.title;
